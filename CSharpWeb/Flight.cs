9d3f857f411745aaedb9693f8a5f9dbbc1b842c5
putrimu <putrimariahulfa@gmail.com>
2018-10-12 13:14:56 +0700

    innitial commit

---
+++ CSharpWeb/Flight.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CSharpWeb
+{
+    public class Flight
+    {
+        public string Flightcode { get; set; }
+
+        public string Depature { get; set; }
+    }
+}
---
+++ CSharpWeb/Schedule.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CSharpWeb.controllers
+{
+    public class Schedule : Controller
+    {
+        public ActionResult Index()
+        {
+            List<Flight>_flight = new List<Flight>()
+                {
+                new Flight { Flightcode = "A-705", Depature = "Amsterdam" },
+                new Flight { Flightcode = "N-344", Depature = "New York" },
+                new Flight { Flightcode = "H-659", Depature = "Hongkong" }
+                };
+            return View(_flight);
+            }
+        }
+
+    }
